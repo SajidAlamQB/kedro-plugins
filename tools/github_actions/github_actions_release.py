75113e225775ca21736dd66969bea3b8e3ed8da8
SajidAlamQB <90610031+SajidAlamQB@users.noreply.github.com>
2023-05-16 15:58:55 +0100

    revert and simplify old way

--- tools/github_actions/github_actions_release.py
+++ tools/github_actions/github_actions_release.py
@@ -5,6 +5,12 @@ import requests
 from pathlib import Path
 
 VERSION_MATCHSTR = r'\s*__version__\s*=\s*"(\d+\.\d+\.\d+)"'
+PACKAGE_PATHS = (
+    "kedro-datasets/kedro_datasets",
+    "kedro-telemetry/kedro_telemetry",
+    "kedro-airflow/kedro_airflow",
+    "kedro-docker/kedro_docker",
+)
 
 
 def get_package_version(base_path, package_path):
@@ -27,25 +33,18 @@ def check_no_version_pypi(pypi_endpoint, package_name, package_version):
 
 if __name__ == "__main__":
     """Check if a package needs to be released"""
-    if len(sys.argv) != 2:
-        print("Please provide package path as an argument.")
-        sys.exit(1)
-
-    env_file = os.getenv('GITHUB_ENV')
-
-    package_path = sys.argv[1]
-    package_name, _ = package_path.split("/")
-
     base_path = Path()
-    package_version = get_package_version(base_path, package_path)
-    pypi_endpoint = f"https://pypi.org/pypi/{package_name}/{package_version}/json/"
+    for package_path in PACKAGE_PATHS:
+        package_name, _ = package_path.split("/")
+        package_version = get_package_version(base_path, package_path)
+        pypi_endpoint = f"https://pypi.org/pypi/{package_name}/{package_version}/json/"
+        env_file = os.getenv('GITHUB_ENV')
 
-    print(package_name, package_version)
-    if check_no_version_pypi(pypi_endpoint, package_name, package_version):
-        print(f"New release needed for {package_name} {package_version}")
-        with open(env_file, "a") as env_file:
-            env_file.write(f"new_release=true\npackage_name={package_name}\npackage_version={package_version}\n")
-    else:
-        print(f"No release needed for {package_name} {package_version}")
-        with open(env_file, "a") as env_file:
-            env_file.write("new_release=false")
+        print(package_name, package_version)
+        if check_no_version_pypi(pypi_endpoint, package_name, package_version):
+            with open(env_file, "a") as env_file:
+                env_file.write(f"new_release=true\npackage_name={package_name}\npackage_version={package_version}\n")
+            break
+        else:
+            with open(env_file, "a") as env_file:
+                env_file.write("new_release=false")
